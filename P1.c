f96ab89f17e0975af640cd54f9471a216495955d
pedrocampisi <42576196+pedrocampisi@users.noreply.github.com>
2018-09-11 20:09:18 -0300

    Update and rename README.md to P1.c

---
+++ P1.c
@@ -0,0 +1,52 @@
+# mtp2018_2
+/*P1.c */
+/* Pedro Paulo Rodrigues Campisi*/
+/* 11811ETE019 */
+#include <stdio.h>
+
+int main()
+{
+	int state=0,i=0;
+	char bits[256];
+	printf ("Digite a sequencia binaria:");
+	scanf("%s",&bits);
+	fflush(stdin);
+	for( i;bits[i]!='\0';i++)
+	{
+		 if(state==0)
+    {
+        if(bits[i]=='0')
+        state=0;
+        else if(bits[i]=='1')
+        state=1;
+    }
+    else if(state==1)
+    {
+        if(bits[i]=='0')
+        state=2;
+        else if(bits[i]=='1')
+        state=0;
+    }
+    else if(state==2)
+    {
+        if(bits[i]=='0')
+        state=1;
+        else if(bits[i]=='1')
+        state=2;
+    }
+    }
+    if(state==0)
+    {
+        printf("%s e multiplo de 3.", bits);
+    }
+    else   
+    {
+       printf("%s nao e multiplo de 3.", bits);
+    }
+    
+    return 0;
+}
+	
+	
+	
+	
