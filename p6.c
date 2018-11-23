248727a79ce742eb2f43347043b60d60dcc228f1
pedrocampisi <42576196+pedrocampisi@users.noreply.github.com>
2018-11-23 19:10:39 -0200

    p6.c

---
+++ p6.c
@@ -0,0 +1,28 @@
+/*P6.c*/
+/*Henrique*/
+/*11811ETE016*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+typedef struct {
+ 	double *x,*y;
+ }Pont;
+int main(){
+	
+	int n,m;
+	Pont p;
+	scanf("%d",&n);
+	fflush(stdin);
+	p.x = calloc(n,sizeof(double ) );
+	p.y= calloc(n,sizeof(double ) );
+	for(m=0;m<n;m++){
+		p.x [m]= cos(m*2.0*M_PI/(n-1));
+        p.y[m] = sin(m*2.0*M_PI/(n-1));
+        
+	}
+	for(m=0;m<n;m++){
+	printf("(%.3lf,%.3lf)\n",p.x[m],p.y[m]);
+	}
+	free(p.x);
+	free(p.y);
+}
